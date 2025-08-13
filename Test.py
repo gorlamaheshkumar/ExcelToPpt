52e49f7fe6a1c1a77ce3c969d7f22fd444eee060
Mahesh Gorla <105862877+gorlamaheshkumar@users.noreply.github.com>
2025-08-13 12:38:44 +0000

    Change

--- Test.py
+++ Test.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import time
+import os
+
+print("--- Python Test Script Starting Up ---")
+print(f"This process will now run forever. Process ID: {os.getpid()}")
+print("You should see a 'Hello world' message in the logs every 10 seconds.")
+print("---------------------------------------------------")
+
+# Infinite loop to keep the container running
+while True:
+    print("Hello world! The script is alive.")
+    # The script will print this message every 10 seconds
+    time.sleep(10)
